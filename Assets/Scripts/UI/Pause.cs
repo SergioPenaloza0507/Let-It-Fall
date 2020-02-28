85f3e4ad220c1e69cc21133a1fb1079feeb5968c
JoseAngelPerez <jose.perezg@upb.edu.co>
2020-02-28 08:48:18 -0500

    Play, pausa y reinicio

---
+++ Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] GameObject toPause;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PauseGame()
+    {
+        //toPause.GetComponent<>().
+    }
+
+    public void ContinueGame()
+    {
+       // toPause.GetComponent<>().
+    }
+
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
