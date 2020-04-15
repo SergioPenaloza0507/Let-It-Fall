25724272e22b1c780dcd63dc561c5c845a05aae1
JoseAngelPerez <jose.perezg@upb.edu.co>
2020-04-15 09:57:06 -0500

    Main menu and leves 1, 2 & 3

---
+++ Assets/Scripts/UI/GoToLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToLevel : MonoBehaviour
+{
+    public void ChangeScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
